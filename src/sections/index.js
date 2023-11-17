632cc0438b7c15a076d7cdfd54c1b0a270542676
don336 <smgmartinez92@gmail.com>
2023-11-17 18:25:43 +0300

    final commit

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import Footer from "./Footer";
+import SpecialOffer from "./SpecialOffer";
+import Services from "./Services";
+import Subscribe from "./Subscribe";
+import SuperQuality from "./SuperQuality";
+import CustomerReviews from "./CustomerReviews";
+
+export {
+  Footer,
+  SpecialOffer,
+  Services,
+  Subscribe,
+  SuperQuality,
+  PopularProducts,
+  Hero,
+  CustomerReviews,
+};
